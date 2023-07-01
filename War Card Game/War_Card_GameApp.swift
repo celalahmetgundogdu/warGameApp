20a9f2609a0f736cfb583f575f0ea742c46d5b49
ahmetcelalgundogdu <celalahmetgundogdu@gmail.com>
2023-07-01 23:12:08 +0300

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Ahmet Celal Gündoğdu on 1.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
